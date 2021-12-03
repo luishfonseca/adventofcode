6f3b44a5947f2ce166c179d7139b80eba5126d14
Luís Fonseca <luis@lhf.pt>
2021-12-03 11:12:40 +0000

    refactor day 3

--- 2021/3/b.cpp
+++ 2021/3/b.cpp
@@ -8,7 +8,6 @@ int solve(vector<string> v, bool swap, int i = 0) {
     if (v.size() == 1) return stoi(v[0], nullptr, 2);
 
     vector<string> z, o;
-    z = o = vector<string>();
 
     for (int j = 0; j < v.size(); j++) {
         if (v[j][i] == '0')
@@ -17,18 +16,13 @@ int solve(vector<string> v, bool swap, int i = 0) {
             o.push_back(v[j]);
     }
 
-    if (z.size() == o.size())
-        return solve(!swap ? o : z, swap, i + 1);
-    else if (o.size() > z.size())
-        return solve(!swap ? o : z, swap, i + 1);
-    else
-        return solve(!swap ? z : o, swap, i + 1);
+    return solve((o.size() >= z.size()) ^ !swap ? o : z, swap, i + 1);
 }
 
 int main() {
-    vector<string> all = vector<string>();
-
+    vector<string> all;
     string in;
+
     while (cin >> in) all.push_back(in);
 
     cout << solve(all, false) * solve(all, true) << endl;
